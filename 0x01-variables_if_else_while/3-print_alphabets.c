c1b61691989e6e8f844b43a43203c76f58d0a944
Abigail-Atoyebi <atoyebi_damilola@yahoo.com>
2023-04-21 13:56:33 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - alphabets
+ *description: 'orints alphabets'
+ *Return: always 0
+ */
+int main(void)
+{
+int n = 97;
+int n = 65;
+
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+while (n <= 90)
+{
+putchar(m);
+m++;
+}
+putchar('\n');
+return (0);
+}
