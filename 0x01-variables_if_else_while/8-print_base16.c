349d63341d3a3e43c4fe9d26e1d4a3563cb414c2
Abigail-Atoyebi <atoyebi_damilola@yahoo.com>
2023-04-21 19:22:37 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,12 +8,12 @@ int main(void)
 {
 int n;
 int m;
-for (n = 48; n <= 57; n++)
+for (n = 48; n <= 58; n++)
 {
 putchar(n);
 n++;
 }
-for (m = 97; m <= 102; m++)
+for (m = 97; m <= 103; m++)
 {
 putchar(m);
 m++;
