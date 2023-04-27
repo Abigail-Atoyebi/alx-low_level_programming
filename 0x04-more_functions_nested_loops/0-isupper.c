2b6bc9b0a51cf74309286a15595eb3fc2978d939
Abigail-Atoyebi <atoyebi_damilola@yahoo.com>
2023-04-27 13:57:54 -0700

    0-

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isupper - checks
+ *@c: character
+ *Return: 1 if its uppercase and 0 if not
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
