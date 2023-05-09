f145548c7c2e4a9c82ede885a5d1c28ff31f39e0
Abigail-Atoyebi <atoyebi_damilola@yahoo.com>
2023-05-09 06:09:12 -0700

    s

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_memcpy - mem
+ *@dest: start 
+ *@src: stop
+ *@n: integer 
+ *Return: value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int r = 0;
+int i = n;
+for (; r < i; r++)
+{
+dest[r] = src[r];
+n--;
+}
+return (dest);
+}
