c0b519d2661c4349dce94e34ab406d6e109c426b
Abigail-Atoyebi <atoyebi_damilola@yahoo.com>
2023-05-17 06:42:12 -0700

    d

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *main - printys
+*@argc: number
+*@argv: array
+ *Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
